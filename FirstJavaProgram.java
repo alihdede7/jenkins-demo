58d98bfe7268209e4ea190f277383326913499a2
Ali Hdedeh <a.hdedeh@QUA-M-ADDAM.midisglobal.local>
2020-06-07 19:22:37 +0300

    second commit

--- FirstJavaProgram.java
+++ FirstJavaProgram.java
@@ -1,5 +1,5 @@
 public class FirstJavaProgram {
   public static void main(String[] args){
-    System.out.println("This is my first program in java 123");
+    System.out.println("This is my first program in java 777");
   }//End of main
 }//End of FirstJavaProgram Class
